54df4269cbcfc29e3a8a260139610d0152c6c494
moreeenaa <moreeemoncerat@gmail.com>
2025-03-10 10:36:35 -0300

    Update if.py

--- if.py
+++ if.py
@@ -1,1 +1,6 @@
-
+#Crear un programa que solicite al usuario ingresar un numero, si el mismo es mayor a 10 mostar por pantalla "Es mayor a 10" en caso contrario mostrar por pantalla "Fin del programa"
+numero = float(input("Ingresa un número: "))
+if numero > 10:
+    print("Es mayor a 10")
+else:
+    print("Fin del programa")
